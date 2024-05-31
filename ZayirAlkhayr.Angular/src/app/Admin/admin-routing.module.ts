4b0dbb60ea5a5238d3d44542297408c71c02cf68
Salam Hany <salam.hany@z2data.com>
2024-05-31 15:34:31 +0300

    add aboutus script and phone

--- ZayirAlkhayr.Angular/src/app/Admin/admin-routing.module.ts
+++ ZayirAlkhayr.Angular/src/app/Admin/admin-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  { path: '', component: AdminComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
--- ZayirAlkhayr.Angular/src/app/app-routing.module.ts
+++ ZayirAlkhayr.Angular/src/app/app-routing.module.ts
@@ -8,12 +8,10 @@ import { PhotosComponent } from './WebSite/photos/photos.component';
 import { ActivityDetailsComponent } from './WebSite/activity-details/activity-details.component';
 import { PhotoDetailsComponent } from './WebSite/photo-details/photo-details.component';
 import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './Admin/admin.component';
-import { AdminModule } from '../app/Admin/admin.module';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'admin', loadChildren: () => import('../app/Admin/admin.module').then(m => m.AdminModule)},
+  { path: 'admin', loadChildren: () => import('../app/Admin/admin.module').then(m => m.AdminModule) },
   {
     path: '', component: HomeComponent, children: [
       { path: 'event', component: EventComponent },
@@ -25,7 +23,8 @@ const routes: Routes = [
       { path: '', redirectTo: 'activity', pathMatch: 'full' }
     ]
   },
-  { path: '', redirectTo: '', pathMatch: 'full' }
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
